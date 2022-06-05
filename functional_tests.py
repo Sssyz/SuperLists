ed988be59c9537dba924d71b843a24371aa85cf8
sssy1202 <784386351@qq.com>
2022-06-05 16:50:50 +0800

    Update functional_tests.py

--- functional_tests.py
+++ functional_tests.py
@@ -7,7 +7,10 @@ class NewVisitorTest(unittest.TestCase):
         self.browser = webdriver.Firefox()
     def tearDown(self):
         self.browser.quit()
-
+    def check_for_row_in_list_table(self,row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text,[row.text for row in rows])
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes#to check out its honepage
         self.browser.get('http://localhost:8000')
@@ -25,22 +28,14 @@ class NewVisitorTest(unittest.TestCase):
         # is tying fly-fishing lures)
         inputbox.send_keys('Use peacock feathers to make a fly')
         # when she hits enter,the page updates，and now the page lists
-        # #"1: Buy peacock feathers" as an iten in a to-do list tableinputbox.send_keys(Keys.ENTER)
+
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        # self.assertTrue(
-        #     any(row.text == '1:Buy peacock feathers' for row in rows),
-        #     f"New to-do item did not appear in table. Contents were:\n{table.text}"
-        # )
-        # self.assertIn('1: Buy peacock feathers',[row.text for row in rows])
 
-        self.assertIn("1: Buy peacock feathers",[row.text for row in rows])
-        self.assertIn(
-            '2: Use peacock feathers to make a fly',
-            [row.text for row in rows]
-        )
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
         self.fail('Finish the test!')
         # She is invited to enter a to-do iten straight away[...rest of conments as before]
 if __name__ == '__main__':
