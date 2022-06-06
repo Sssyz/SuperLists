ec6ff9dc9e070b09cf3f3d2eea4dc7eaf7f4e145
sssy1202 <784386351@qq.com>
2022-06-06 17:33:06 +0800

    URLS for forms x2,moved code in views+tests,new URL

--- functional_tests/functional_tests.py
+++ functional_tests/functional_tests.py
@@ -28,34 +28,34 @@ class NewVisitorTest(LiveServerTestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text,[row.text for row in rows])
-    def test_can_start_a_list_and_retrieve_it_later(self):
-        # Edith has heard about a cool new online to-do app. She goes#to check out its honepage
-        self.browser.get(self.live_server_url)
-        # She notices the page title and header nention to-do lists
-        self.assertIn('To-Do', self.browser.title)
-        header_text = self.browser.find_element_by_tag_name('h1').text
-        self.assertIn('To-Do', header_text)
-
-        # She is invited to enter a to-do iten straight away
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertEqual(
-            inputbox.get_attribute('placeholder'),
-            'Enter a to-do item')
-        # She types "Buy peacock feathers" into a text box (Edith 's hobby
-        # is tying fly-fishing lures)
-        inputbox.send_keys('Buy peacock feathers')
-        inputbox.send_keys(Keys.ENTER)
-        time.sleep(2)
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys('Use peacock feathers to make a fly')
-        inputbox.send_keys(Keys.ENTER)
-        time.sleep(2)
-        # when she hits enter,the page updates，and now the page lists
-
-        self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
-        self.wait_for_row_in_list_table('1: Buy peacock feathers')
-        self.fail('Finish the test!')
-        # She is invited to enter a to-do iten straight away[...rest of conments as before]
+    # def test_can_start_a_list_and_retrieve_it_later(self):
+    #     # Edith has heard about a cool new online to-do app. She goes#to check out its honepage
+    #     self.browser.get(self.live_server_url)
+    #     # She notices the page title and header nention to-do lists
+    #     self.assertIn('To-Do', self.browser.title)
+    #     header_text = self.browser.find_element_by_tag_name('h1').text
+    #     self.assertIn('To-Do', header_text)
+    #
+    #     # She is invited to enter a to-do iten straight away
+    #     inputbox = self.browser.find_element_by_id('id_new_item')
+    #     self.assertEqual(
+    #         inputbox.get_attribute('placeholder'),
+    #         'Enter a to-do item')
+    #     # She types "Buy peacock feathers" into a text box (Edith 's hobby
+    #     # is tying fly-fishing lures)
+    #     inputbox.send_keys('Buy peacock feathers')
+    #     inputbox.send_keys(Keys.ENTER)
+    #     time.sleep(2)
+    #     inputbox = self.browser.find_element_by_id('id_new_item')
+    #     inputbox.send_keys('Use peacock feathers to make a fly')
+    #     inputbox.send_keys(Keys.ENTER)
+    #     time.sleep(2)
+    #     # when she hits enter,the page updates，and now the page lists
+    #
+    #     self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
+    #     self.wait_for_row_in_list_table('1: Buy peacock feathers')
+    #     self.fail('Finish the test!')
+    #     # She is invited to enter a to-do iten straight away[...rest of conments as before]
 
 
     def test_multiple_users_can_start_lists_at_different_urls(self):
--- lists/views.py
+++ lists/views.py
@@ -3,10 +3,6 @@ from lists.models import Item
 from django.http import  HttpResponse
 # Create your views here.
 def home_page(request):
-    if request.method == 'POST':
-        new_item_text = request.POST['item_text']
-        Item.objects.create(text=new_item_text)
-        return redirect('/lists/the-only-list-in-the-world/')
 
     return render(request,'home.html')
 def view_list(request):
@@ -18,3 +14,6 @@ def view_list(request):
         new_item_text = ''
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world/')
