fb4d3e1d00df656f35e3c0501ba66f53be45a09e
sssy1202 <784386351@qq.com>
2022-06-06 16:24:19 +0800

    add wait_for_row_in_list_table

--- functional_tests/functional_tests.py
+++ functional_tests/functional_tests.py
@@ -2,8 +2,24 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from django.test import LiveServerTestCase
 import time
+from selenium.common.exceptions import WebDriverException
+MAX_WAIT = 10
 import unittest
 class NewVisitorTest(LiveServerTestCase):
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text,[row.text for row in rows])
+                return
+            except (AssertionError,WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
+
+
     def setUp(self):
         self.browser = webdriver.Firefox()
     def tearDown(self):
@@ -36,13 +52,8 @@ class NewVisitorTest(LiveServerTestCase):
         time.sleep(2)
         # when she hits enter,the page updates，and now the page lists
 
-
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
-        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
         self.fail('Finish the test!')
         # She is invited to enter a to-do iten straight away[...rest of conments as before]
 
