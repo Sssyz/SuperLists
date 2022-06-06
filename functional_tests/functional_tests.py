4e1180d591ac7d396a52748fdc34e2d02d4dc2ac
sssy1202 <784386351@qq.com>
2022-06-06 16:08:26 +0800

    make function_tests an app, use LiveServerTestCase

--- functional_tests/functional_tests.py
+++ functional_tests/functional_tests.py
@@ -1,8 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from django.test import LiveServerTestCase
 import time
 import unittest
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
     def tearDown(self):
@@ -13,7 +14,7 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn(row_text,[row.text for row in rows])
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes#to check out its honepage
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
         # She notices the page title and header nention to-do lists
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
@@ -28,12 +29,15 @@ class NewVisitorTest(unittest.TestCase):
         # is tying fly-fishing lures)
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
+        time.sleep(2)
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(2)
         # when she hits enter,the page updates，and now the page lists
 
-        inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+
+
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
 
@@ -41,7 +45,4 @@ class NewVisitorTest(unittest.TestCase):
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
         self.fail('Finish the test!')
         # She is invited to enter a to-do iten straight away[...rest of conments as before]
-if __name__ == '__main__':
-    unittest.main()
-
 
